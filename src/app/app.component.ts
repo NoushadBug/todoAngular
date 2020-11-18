42a648ea1adba3fbfdadd4c911418dd0ac589e98
Passworderror29 <iflamegooglic@gmail.com>
2020-11-18 19:10:48 +0600

    all functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { Todo } from '../app/todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoAngular';
+  todoValue:string;
+  list: Todo[];
+
+  constructor(){
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem(){
+    if (this.todoValue !== ''){
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+
+  deleteItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
